6fa6e4e95a26f22780d041c2f3d9835b2b02b088
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-07-01 06:27:54 +0530

    Express In Memory Kidney Hospital Practice Project

---
+++ Express In Memory Kidney Hospital Project/index.js
@@ -0,0 +1,14 @@
+// You need to create 4 routes (4 things that the hospital can do)
+// 1. GET - User can check how many kidneys they have and their health
+// 2. POST - User can add a new kidney
+// 3. PUT - User can replace a kidney, make it healthy
+// 4. DELETE - User can remove a kidney
+
+// 1. What should happen if they try to delete when there are no kidneys?
+// 2. What should happen if they try to make a kidney healthy when all are already healthy
+
+// Assume There is only a single user John
+
+
+/* In Memory Kidney Hospital */
+
